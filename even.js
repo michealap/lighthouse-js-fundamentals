faa1c1c298743b02a747d227ee3e16464ce41644
Micheala Palmer <michealapalmer@gmail.com>
2021-08-30 07:22:35 +0000

    check for even numbers function and passing the returned result directly to the console

---
+++ even.js
@@ -0,0 +1,11 @@
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+console.log(isEven(10));
+console.log(isEven(11));
+/* Expanded version :
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);*/
